f1f058c79c3b1e8ba4a9d3f3457ab092c93746c7
Marcelo Kriiger <marcelokriiger@outlook.com>
2023-12-10 02:38:01 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-questions]')
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
 
 
+    //Comportamento de sumir o header quando der scroll
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementosDoHeader()
+        }else{
+            exibeElementosDoHeader()
+        }
+    })
+
+    //Programação das abas
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton
@@ -10,10 +25,21 @@ document.addEventListener('DOMContentLoaded', function(){
             aba.classList.add('shows__list--is-active')
             removeBotaoAtivo()
             botao.target.classList.add('shows__tabs__button--is-active')
-            console.log('alo')
         })
     }
+
+    //FAQ
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is--open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]')
@@ -29,4 +55,16 @@ function escondeTodasAbas(){
     for(let i = 0; i < tabsContainer.length; i++){
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
+}
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header')
+
+    header.classList.add('header--is--hidden')
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header')
+
+    header.classList.remove('header--is--hidden')
 }
